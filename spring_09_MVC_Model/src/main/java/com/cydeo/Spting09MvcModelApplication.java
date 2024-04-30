6cd572f1b2a55c67893a187de1382f7ee81853b6
majastamenkovic <majastam1996@icloud.com>
2024-04-30 11:59:55 -0400

    MVC Model

---
+++ spring_09_MVC_Model/src/main/java/com/cydeo/Spting09MvcModelApplication.java
@@ -0,0 +1,16 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Spring09MvcModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring09MvcModelApplication.class, args);
+
+
+	}
+
+
+}
---
+++ spring_09_MVC_Model/src/test/java/com/cydeo/Spting09MvcModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spting09MvcModelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
